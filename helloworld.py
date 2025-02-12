f47b1cb004fbfe2d629573e081c3616eacd71639
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2025-02-12 08:59:19 +0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,13 +1,3 @@
 
 print("Hello World")
 
-# 🔥 Why This Project?
-# First step in learning Python 🐍
-# Understanding how to execute Python scripts
-# Setting up a GitHub repository for practice 💡
-
-# 📌 Contribute
-# This is a simple project, but feel free to fork it and add enhancements! 😊
-
-# 📜 License
-# This project is open-source and available under the MIT License.
